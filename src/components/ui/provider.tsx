e5cfd0b07c60b41c73201e0e324388bf32fc2048
Justin Williams <justin@catalysttek.com>
2025-08-08 23:04:18 -0700

    Install Chakra UI

---
+++ src/components/ui/provider.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { ChakraProvider } from "@chakra-ui/react";
+import { ColorModeProvider, type ColorModeProviderProps } from "./color-mode";
+
+export function Provider(props: ColorModeProviderProps) {
+  return (
+    <ChakraProvider>
+      <ColorModeProvider {...props} />
+    </ChakraProvider>
+  );
+}
