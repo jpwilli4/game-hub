d5f23150c472ae8625a91470784722d95031ecba
Justin Williams <justin@catalysttek.com>
2025-08-09 21:40:38 -0700

    create custom hook to fetch games

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,27 +1,8 @@
-import apiClient from "@/services/api-client";
-import React, { useEffect, useState } from "react";
 import { Text } from "@chakra-ui/react";
-
-interface FetchGameResponse {
-  count: number;
-  results: Game[];
-}
-
-interface Game {
-  id: number;
-  name: string;
-}
+import useGames from "@/hooks/useGames";
 
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    apiClient
-      .get<FetchGameResponse>("/games")
-      .then((res) => setGames(res.data.results))
-      .catch((err) => setError(err.message));
-  }, []);
+  const { games, error } = useGames();
 
   return (
     <>
---
+++ src/hooks/useGames.ts
@@ -0,0 +1,35 @@
+import apiClient from "@/services/api-client";
+import { CanceledError } from "axios";
+import { useEffect, useState } from "react";
+
+interface FetchGameResponse {
+  count: number;
+  results: Game[];
+}
+
+interface Game {
+  id: number;
+  name: string;
+}
+
+const useGames = () => {
+const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const controller = new AbortController()
+
+    apiClient
+      .get<FetchGameResponse>("/games", { signal: controller.signal})
+      .then((res) => setGames(res.data.results))
+      .catch((err) => {
+        if(err instanceof CanceledError) return;
+        setError(err.message)});
+
+    return () => controller.abort();
+  }, []);
+
+  return { games, error };
+}
+
+export default useGames;
