dd403a8fa425ba0e0c7384a4c5629433a5048b2b
Justin Williams <justin@catalysttek.com>
2025-08-10 16:20:29 -0700

    get optimized images

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,4 +1,5 @@
 import type { Game } from "@/hooks/useGames";
+import getCroppedImageUrl from "@/services/image-url";
 import {
   Badge,
   Card,
@@ -18,7 +19,7 @@ interface Props {
 const GameCard = ({ game }: Props) => {
   return (
     <Card borderRadius={10} overflow="hidden">
-      <Image src={game.background_image} />
+      <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
         <HStack justifyContent="space-between">
