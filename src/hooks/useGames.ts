6d67c6c9e574716297923e0ce1da4c8256c68504
Justin Williams <jdubbromeo@gmail.com>
2025-08-14 11:13:23 -0700

    refactoring: create a reusable api client

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,8 +1,9 @@
 import type { GameQuery } from "@/App";
-import { type FetchResponse } from "../services/api-client";
+import APIClient, { type FetchResponse } from "../services/api-client";
 import { useQuery } from "@tanstack/react-query";
-import apiClient from "@/services/api-client";
 import type { Platform } from "./usePlatforms";
+
+const apiClient = new APIClient<Game>('/games');
 export interface Game {
   id: number;
   name: string;
@@ -17,15 +18,14 @@ const useGames = (gameQuery: GameQuery) =>
     queryKey: ['games', gameQuery],
     queryFn: () => 
       apiClient
-        .get<FetchResponse<Game>>('/games', {
+        .getAll({
           params: {
             genres: gameQuery.genre?.id, 
             parent_platforms: gameQuery.platform?.id,
             ordering: gameQuery.sortOrder,
             search: gameQuery.searchText
-          },
-        })
-        .then(res => res.data)
+          }
+        }) 
     })
 
 export default useGames;
--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,8 +1,10 @@
 // import genres from '../data/genres';
 import { useQuery } from '@tanstack/react-query';
 import { type FetchResponse } from '../services/api-client';
-import apiClient from '@/services/api-client';
+import APIClient from '@/services/api-client';
 import genres from '@/data/genres';
+
+const apiClient = new APIClient<Genre>('/genres');
 
 export interface Genre {
     id: number,
@@ -10,11 +12,11 @@ export interface Genre {
     image_background: string
 }
 
-const useGenres = () => useQuery<Genre[]>({
+const useGenres = () => useQuery({
     queryKey: ['genres'],
-    queryFn: () => apiClient.get<FetchResponse<Genre>>('/genres').then(res => res.data.results),
+    queryFn: apiClient.getAll,
     staleTime: 24 * 60 * 60 * 1000, //24 hours
-    initialData: genres
+    initialData: { count: genres.length, results: genres }
 })
 
 // const useGenres = () => ({data: genres, isLoading: false, error: null});//useData<Genre>("/genres");
