31ede7abac925095d5c438e18078b10c6060240f
Justin Williams <jdubbromeo@gmail.com>
2025-08-14 09:55:25 -0700

    refactoring to use react-query

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,4 +1,8 @@
-import genres from '../data/genres';
+// import genres from '../data/genres';
+import { useQuery } from '@tanstack/react-query';
+import { type FetchResponse } from '../services/api-client';
+import apiClient from '@/services/api-client';
+import genres from '@/data/genres';
 
 export interface Genre {
     id: number,
@@ -6,6 +10,13 @@ export interface Genre {
     image_background: string
 }
 
-const useGenres = () => ({data: genres, isLoading: false, error: null});//useData<Genre>("/genres");
+const useGenres = () => useQuery<Genre[]>({
+    queryKey: ['genres'],
+    queryFn: () => apiClient.get<FetchResponse<Genre>>('/genres').then(res => res.data.results),
+    staleTime: 24 * 60 * 60 * 1000, //24 hours
+    initialData: genres
+})
+
+// const useGenres = () => ({data: genres, isLoading: false, error: null});//useData<Genre>("/genres");
 
 export default useGenres;
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,22 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import App from "./App.tsx";
 import theme from "./theme.ts";
 import "./index.css";
+import React from "react";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+  <React.StrictMode>
     <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </ChakraProvider>
-  </StrictMode>
+  </React.StrictMode>
 );
